2fa3c6ed497282e05112e52901d0f93105f65bda
MariaLauraElias <eliasmarialaura@gmail.com>
2022-08-29 09:48:20 -0300

    cambios en product item y product detail

--- SitioWebEntrelineas/routes/product-router.js
+++ SitioWebEntrelineas/routes/product-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product-controller');
 
-router.get('/product/:id', productController.detail);
+router.get('/product-detail/:id', productController.detail);
 router.get('/product-cart', productController.cart)
 router.get('/product-edition/:id', productController.editar)
 
