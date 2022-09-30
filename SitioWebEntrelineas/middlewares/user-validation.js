6106061c12db642d98e0a1d87dde8326cd263ffc
MariaLauraElias <eliasmarialaura@gmail.com>
2022-09-30 16:23:44 -0300

    corrijo algunas sintaxis

--- SitioWebEntrelineas/middlewares/user-validation.js
+++ SitioWebEntrelineas/middlewares/user-validation.js
@@ -7,11 +7,11 @@ const validateRegister = [
     .isString().withMessage('¡Solo letras, eh!'),
     body('user').notEmpty().withMessage('¡Olvidaste escribir un nombre de usuario!').bail()
     .isLength({min: 5}).withMessage('Debe tener al menos cinco caracteres').bail()
-    .isLength({max: 10}).withMessage('Con menos de diez caracteres es suficiente').bail(),
+    .isLength({max: 10}).withMessage('Con menos de diez caracteres es suficiente'),
     body('email').notEmpty().withMessage('¡Olvidaste escribir un email!').bail()
     .isEmail().withMessage('¡Tiene que ser un email válido!'),
     body('password').notEmpty().withMessage('¡Olvidaste escribir una contraseña!').bail()
-    .isLength({min: 6}).withMessage('Debe tener al menos seis caracteres').bail()
+    .isLength({min: 6}).withMessage('Debe tener al menos seis caracteres')
 ];
 
 module.exports = validateRegister
--- SitioWebEntrelineas/routes/user-router.js
+++ SitioWebEntrelineas/routes/user-router.js
@@ -4,13 +4,13 @@ const multer = require('multer');
 const validationRegister = require('../middlewares/user-validation');
 
 
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "public/images/users");
     },
     filename: function (req, file, cb) {
       console.log({ file });
-      cb(null, Date.now() + "" + file.originalname);
+      cb(null, Date.now() + "-" + file.originalname);
     },
   });
   
@@ -20,7 +20,7 @@ const userController = require('../controllers/user-controller');
 
 router.get('/register', userController.register);
 
-router.post('/register', upload.single('avatar'), validationRegister, userController.registerProsses);
+router.post('/register',  validationRegister, upload.single('avatar'), userController.registerProsses);
 
 router.get('/login', userController.login);
 
