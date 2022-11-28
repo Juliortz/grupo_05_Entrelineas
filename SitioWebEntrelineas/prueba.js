901c00f566a04a8ff62ab87af25278ec14d16ab5
MariaLauraElias <eliasmarialaura@gmail.com>
2022-11-27 21:12:26 -0300

    prueba de react

--- SitioWebEntrelineas/prueba.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-let contraseniaEncriptado = bcrypt.hashSync("123456", 10)
-let isConstraseniaOk = bcrypt.compareSync("123456", contraseniaEncriptado);
-console.log(isConstraseniaOk);
-let numero = 12345
-console.log('"'+numero + '"');
