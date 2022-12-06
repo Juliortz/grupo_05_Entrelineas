324f5517865c92fac2698596748ae70ff402244d
Mateo Herrera Tamayo <teoherrera07@gmail.com>
2022-12-05 19:05:25 -0500

    Creo api last user

--- SitioWebEntrelineas/Back-end/routes/apisRoutes/api-user-router.js
+++ SitioWebEntrelineas/Back-end/routes/apisRoutes/api-user-router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const apiUserController = require('../../controllers/apisControllers/api-user-controller');
 
+router.get('/last', apiUserController.last);
 router.get('/', apiUserController.list);
 router.get('/:id', apiUserController.detail);
+
 
 module.exports = router
